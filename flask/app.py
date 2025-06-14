4f927abe7740a33a8a10c49824f2125acd6acccb
unknown <LanvAI.com>
2025-06-14 10:26:03 +0300

    changes in back folder

--- flask/app.py
+++ flask/app.py
@@ -956,5 +956,5 @@ if __name__ == '__main__':
     print(app.url_map)
 
     # Run the Flask app on all interfaces
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
 
